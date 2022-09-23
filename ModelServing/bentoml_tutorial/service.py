7b17c41bd438fed9d0f58648e7fe4f2bd322b2bc
Tolgaisikp <tolga199834@gmail.com>
2022-09-23 20:03:02 +0300

    added bentoml and copy mlflow

---
+++ ModelServing/bentoml_tutorial/service.py
@@ -0,0 +1,15 @@
+import bentoml
+from bentoml.io import NumpyNdarray
+
+import numpy as np
+
+import warnings
+warnings.filterwarnings("ignore")
+
+iris_clf_runner = bentoml.sklearn.get("iris_clf:latest").to_runner()
+svc = bentoml.Service("iris_classifier", runners=[iris_clf_runner])
+
+@svc.api(input=NumpyNdarray(), output=NumpyNdarray())
+def classify(input_series: np.ndarray) -> np.ndarray:
+    result = iris_clf_runner.predict.run(input_series)
+    return result
